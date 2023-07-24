bc67b9c6232c7e1a394f28bbcdd231d076cfe8a7
Kodi Bharadwaj <138383233+KodiBharadwaj@users.noreply.github.com>
2023-07-24 20:24:30 +0530

    Update SingleChat.js

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -13,7 +13,7 @@ import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://mern-chat-messenger.onrender.com";
 var socket, selectedChatCompare;
 
 
